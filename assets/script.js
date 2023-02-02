38064ef4479585a8033e0b91bfdd332c46a5fbdf
Ramaz-Didi <117308754+Ramaz-Didi@users.noreply.github.com>
2023-02-02 17:51:37 +0000

    all working except history buttons responce

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ let currentEl=document.querySelector("#current");
 var iconEl = document.createElement("img");
 var forecastIconEl=[]; 
 var historyButtonEl=[]
-if(localStorage.getItem("searchedCities") !== "null"){
+if ( localStorage.length >0 ){
   var searchedCities = JSON.parse(localStorage.getItem("searchedCities"));
   }
 for (let i = 0; i <= 5; i++) {
